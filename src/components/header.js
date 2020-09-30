bfc3a4371129351460c8f3d9669f7219aeec65b3
olcaneristi <ogulcaneristi123@gmail.com>
2020-09-30 05:56:37 +0300

    mobile icin hamburger menu eklendi

--- src/components/header.js
+++ src/components/header.js
@@ -1,47 +1,53 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import Logo from "./assets/images/logo.svg"
+import { IoMdClose, IoMdMenu } from "react-icons/io"
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <a href="/#">
-          <img src={Logo} alt="logo" />
-        </a>
-        <nav>
-          <ul className="nav_links">
-            <li className="header-li">
-              <a className="header-a" href="/#">
-                Home
-              </a>
-            </li>
-            <li className="header-li">
-              <a className="header-a" href="/#">
-                About
-              </a>
-            </li>
-            <li className="header-li">
-              <a className="header-a" href="/#">
-                Contact
-              </a>
-            </li>
-            <li className="header-li">
-              <a className="header-a" href="/#">
-                Blog
-              </a>
-            </li>
-            <li className="header-li">
-              <a className="header-a" href="/#">
-                Careers
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <button className="invitebtn" type="submit">
-          Request Invite
-        </button>
-      </header>
-    )
-  }
+function Header() {
+  const [sidebar, setSidebar] = useState(false)
+  const showSidebar = () => setSidebar(!sidebar)
+
+  return (
+    <header>
+      <a href="/#">
+        <img src={Logo} alt="logo" />
+      </a>
+      <button type="button" className="sidebar-cta" onClick={showSidebar}>
+        {sidebar ? <IoMdClose /> : <IoMdMenu />}
+      </button>
+      <nav className={sidebar ? "navbar-menu active" : "navbar-menu"}>
+        <ul className="nav_links" onClick={showSidebar}>
+          <li className="header-li">
+            <a className="header-a" href="/#">
+              Home
+            </a>
+          </li>
+          <li className="header-li">
+            <a className="header-a" href="/#">
+              About
+            </a>
+          </li>
+          <li className="header-li">
+            <a className="header-a" href="/#">
+              Contact
+            </a>
+          </li>
+          <li className="header-li">
+            <a className="header-a" href="/#">
+              Blog
+            </a>
+          </li>
+          <li className="header-li">
+            <a className="header-a" href="/#">
+              Careers
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <button className="invitebtn" type="submit">
+        Request Invite
+      </button>
+    </header>
+  )
 }
+
 export default Header
